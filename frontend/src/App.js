f63791ff4cff05d1f20b52078ca1e081d97ea0a8
Jonas Kirch <jonaskirch9@gmail.com>
2019-10-31 18:10:40 -0300

    commit inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Routes from "./router";
+
+import Logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <div className="container">
+      <img src={Logo} alt="AirCnc" />
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
