8b59feb7b4f2e2532e282d88b13440171fcad928
Jonas Kirch <jonaskirch9@gmail.com>
2019-12-29 15:10:39 -0300

    .

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -11,7 +11,7 @@ export default function Dashboard() {
   const user_id = localStorage.getItem("user");
   const socket = useMemo(
     () =>
-      socketio("http://192.168.0.5:8000", {
+      socketio("http://localhost:8000", {
         query: { user_id }
       }),
     [user_id]
